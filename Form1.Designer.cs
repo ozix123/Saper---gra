3b2b28c8785203823d8fad048dabba5bb96f0af8
ozix123 <oskar.rytwinski@wp.pl>
2022-03-10 20:59:09 +0100

    Usunięto "Przycisk 3"

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
@@ -98,21 +97,11 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "0";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(374, 206);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(145, 38);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Przycisk 3";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(593, 280);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -134,7 +123,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
     }
 }
 
